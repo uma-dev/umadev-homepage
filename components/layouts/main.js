18938031c5a121d504fb207639440e8e4c078f9f
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-02-07 19:02:29 -0600

    Fixed typos

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -7,7 +7,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Omar Roldan - Homepage</title>
+        <title>Omar - Homepage</title>
       </Head>
 
       <Navbar path={router.asPath} />
--- components/logo.js
+++ components/logo.js
@@ -28,7 +28,7 @@ const Logo = () => {
         <BulbIcon />
         <Text
           color={useColorModeValue("gray.800", "whiteAlpha.900")}
-          fontFamily='M PLUS Rounded 1c", sans-serif'
+          fontFamily="M PLUS Rounded 1c, sans-serif"
           fontWeight="bold"
           ml={3}
         >
