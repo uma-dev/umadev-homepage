f4198a8ccacee4f4a299245612480930a276d564
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-02-10 18:31:39 -0600

    Fixed boxes

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ const Page = () => {
         <Box display={{ md: "flex" }}>
           <Box flexGrow={1}>
             <Heading as="h2" variant="page-title">
-              Omar Roldan
+              Omar Roldán
             </Heading>
             <p>Developer / Telecomm Engineer / Tech lover</p>
           </Box>
@@ -63,109 +63,108 @@ const Page = () => {
               />
             </Box>
           </Box>
-
-          <Section delay={0.1}>
-            <Heading as="h3" variant="section-title">
-              Work
-            </Heading>
-            <Paragraph>
-              Accountable developer with knowledge in Spring Boot, Spring REST,
-              Spring Security, GCP, SQL, HTML/CSS and data analytics. I'm
-              seeking about collaborating on intricate software challenges to
-              create enterprise-level applications, acquire new skills and
-              thrive through collaboration with other developers. Reach me at{" "}
-              <NextLink href="https://www.linkedin.com/in/omar-roldan-guerra/">
-                <Link>LinkedIn</Link>
-              </NextLink>
-              .
-            </Paragraph>
-            <Box align="center" my={4}>
-              <NextLink href="/works">
-                <Button rightIcon={<ChevronRightIcon />} colorScheme="telegram">
-                  My portafolio
-                </Button>
-              </NextLink>
-            </Box>
-          </Section>
-
-          <Section delay={0.2}>
-            <Heading as="h3" variant="section-title">
-              Bio
-            </Heading>
-            <BioSection>
-              <BioYear>1997</BioYear>
-              Born in Mexico.
-            </BioSection>
-            <BioSection>
-              <BioYear>2020</BioYear>
-              Completed the Telecommunications Program in the School of
-              Engineering at UNAM (CU).
-            </BioSection>
-            <BioSection>
-              <BioYear>2020</BioYear>
-              Worked at IINGEN UNAM (Scholarship holder)
-            </BioSection>
-            <BioSection>
-              <BioYear>2021</BioYear>
-              Worked at IFT (techical and fixed networks).
-            </BioSection>
-            <BioSection>
-              <BioYear>2022</BioYear>
-              Worked at IFT (mobile networks such as 5G, selfdriven cars, etc.
-              ).
-            </BioSection>
-
-            <BioSection>
-              <BioYear>2023 - present</BioYear>
-              Completed different courses related to Java, Spring, Micro
-              services, RDB, Frontend, DT, etc.
-            </BioSection>
-          </Section>
-
-          <Section delay={0.3}>
-            <Heading as="h3" variant="section-title">
-              On the web
-            </Heading>
-            <List>
-              <ListItem>
-                <Link href="https://github.com/uma-dev" target="_blank">
-                  <Button
-                    variant="ghost"
-                    colorScheme="telegram"
-                    leftIcon={<IoLogoGithub />}
-                  >
-                    @uma-dev
-                  </Button>
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link href="https://twitter.com/uma_dev_" target="_blank">
-                  <Button
-                    variant="ghost"
-                    colorScheme="telegram"
-                    leftIcon={<IoLogoTwitter />}
-                  >
-                    @uma_dev_
-                  </Button>
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link
-                  href="https://www.linkedin.com/in/omar-roldan-guerra/"
-                  target="_blank"
-                >
-                  <Button
-                    variant="ghost"
-                    colorScheme="telegram"
-                    leftIcon={<IoLogoLinkedin />}
-                  >
-                    /omar-roldan-guerra
-                  </Button>
-                </Link>
-              </ListItem>
-            </List>
-          </Section>
         </Box>
+
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            Accountable developer with knowledge in Spring Boot, Spring REST,
+            Spring Security, GCP, SQL, HTML/CSS and data analytics. I'm seeking
+            about collaborating on intricate software challenges to create
+            enterprise-level applications, acquire new skills and thrive through
+            collaboration with other developers. Reach me at{" "}
+            <NextLink href="https://www.linkedin.com/in/omar-roldan-guerra/">
+              <Link>LinkedIn</Link>
+            </NextLink>
+            .
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="telegram">
+                My portafolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1997</BioYear>
+            Born in Mexico.
+          </BioSection>
+          <BioSection>
+            <BioYear>2020</BioYear>
+            Completed the Telecommunications Program in the School of
+            Engineering at UNAM (CU).
+          </BioSection>
+          <BioSection>
+            <BioYear>2020</BioYear>
+            Worked at IINGEN UNAM (Scholarship holder)
+          </BioSection>
+          <BioSection>
+            <BioYear>2021</BioYear>
+            Worked at IFT (techical and fixed networks).
+          </BioSection>
+          <BioSection>
+            <BioYear>2022</BioYear>
+            Worked at IFT (mobile networks such as 5G, selfdriven cars, etc. ).
+          </BioSection>
+
+          <BioSection>
+            <BioYear>2023 - present</BioYear>
+            Completed different courses related to Java, Spring, Micro services,
+            RDB, Frontend, DT, etc.
+          </BioSection>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            On the web
+          </Heading>
+          <List>
+            <ListItem>
+              <Link href="https://github.com/uma-dev" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="telegram"
+                  leftIcon={<IoLogoGithub />}
+                >
+                  @uma-dev
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://twitter.com/uma_dev_" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="telegram"
+                  leftIcon={<IoLogoTwitter />}
+                >
+                  @uma_dev_
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link
+                href="https://www.linkedin.com/in/omar-roldan-guerra/"
+                target="_blank"
+              >
+                <Button
+                  variant="ghost"
+                  colorScheme="telegram"
+                  leftIcon={<IoLogoLinkedin />}
+                >
+                  /omar-roldan-guerra
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+        </Section>
       </Container>
     </Layout>
   );
