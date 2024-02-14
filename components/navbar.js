502283cd3ad62f2f39fc24fd27a0261c710c2e31
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-02-13 19:32:34 -0600

    Removed unnecesary links

--- components/navbar.js
+++ components/navbar.js
@@ -71,9 +71,6 @@ const Navbar = (props) => {
           <LinkItem href="/works" path={path}>
             Works
           </LinkItem>
-          <LinkItem href="/posts" path={path}>
-            Posts
-          </LinkItem>
         </Stack>
 
         <Box flex={1} align="right">
@@ -92,9 +89,6 @@ const Navbar = (props) => {
                 </NextLink>
                 <NextLink href="/works" passHref>
                   <MenuItem as={Link}>Works</MenuItem>
-                </NextLink>
-                <NextLink href="/posts" passHref>
-                  <MenuItem as={Link}>Posts</MenuItem>
                 </NextLink>
               </MenuList>
             </Menu>
