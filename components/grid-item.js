b02ec7b18bbec2756dd139ec95da5357bc0c49d5
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-03-20 16:37:01 -0600

    feat(works): added acordion to display details

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,7 +1,21 @@
-import NextLink from "next/link";
 import Image from "next/image";
-import { Box, Text, LinkBox, LinkOverlay } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Link,
+  LinkBox,
+  LinkOverlay,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Text,
+} from "@chakra-ui/react";
 import { Global } from "@emotion/react";
+import { useRef } from "react";
+import { IoLogoGithub } from "react-icons/io5";
 
 export const GridItem = ({ children, href, title, thumbnail }) => (
   <Box w="100%" textAlign="center">
@@ -21,39 +35,68 @@ export const GridItem = ({ children, href, title, thumbnail }) => (
   </Box>
 );
 
-export const WorkGridItem = ({
-  children,
-  category = "works",
-  id,
-  title,
-  href,
-  thumbnail,
-}) => (
-  <Box w="100%" textAlign="center">
-    <LinkBox
-      as={NextLink}
-      // href={`/${category}/${id}`}
-      href={href}
-      rel="noopener noreferrer"
-      target="_blank"
-      scroll={false}
-      cursor="pointer"
-    >
-      <Image
-        src={thumbnail}
-        alt={title}
-        className="grid-item-thumbnail"
-        placeholder="blur"
-      />
-      <LinkOverlay as="div" href={`/${category}/${id}`}>
-        <Text mt={2} fontSize={20}>
-          {title}
+export const WorkGridItem = ({ children, title, href, thumbnail }) => {
+  const initialFocusRef = useRef();
+  return (
+    <Box w="100%">
+      <LinkBox as="a" href="#" scroll={false} cursor="pointer">
+        <Image
+          src={thumbnail}
+          alt={title}
+          className="grid-item-thumbnail"
+          placeholder="blur"
+        />
+        <LinkOverlay as="div">
+          <Text mt={2} fontSize={20} align="center">
+            {title}
+          </Text>
+        </LinkOverlay>
+        <Text fontSize={14} align="left">
+          {children}
         </Text>
-      </LinkOverlay>
-      <Text fontSize={14}>{children}</Text>
-    </LinkBox>
-  </Box>
-);
+      </LinkBox>
+
+      <Accordion allowToggle>
+        <AccordionItem border="none">
+          <h2>
+            <AccordionButton>
+              <Box
+                as="span"
+                flex="1"
+                textAlign="center"
+                fontSize="sm"
+                color="grey"
+              >
+                see more
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4} p={1}>
+            <Flex direction="column" gap={5}>
+              <Text fontSize={14}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </Text>
+              <Link href={href} target="_blank" alignSelf="flex-end">
+                <Button
+                  colorScheme="telegram"
+                  leftIcon={<IoLogoGithub />}
+                  size="sm"
+                  ref={initialFocusRef}
+                >
+                  Code
+                </Button>
+              </Link>
+            </Flex>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Box>
+  );
+};
 
 export const GridItemStyle = () => (
   <Global
